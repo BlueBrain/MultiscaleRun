47f487f636b9b9d44940d099545ab64260eb5dac
Christos Kotsalos <christos.kotsalos@epfl.ch>
2022-09-14 13:35:17 +0200

    CI : WIP (start with pure dualrun)

--- .ci/test_dualrun.sh
+++ .ci/test_dualrun.sh
@@ -5,7 +5,7 @@ set -e
 echo "! setup !"
 
 module purge
-module load unstable python-dev py-neurodamus py-mpi4py
+module load unstable git python-dev py-neurodamus py-mpi4py
 module load intel gcc hpe-mpi
 
 echo "*******************************************************************************"
