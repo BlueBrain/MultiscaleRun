92da9ddc8d168ef2451134339cc413726569c847
Christos Kotsalos <christos.kotsalos@epfl.ch>
2022-09-14 11:12:30 +0200

    CI : WIP (start with pure dualrun)

--- .ci/test_dualrun.sh
+++ .ci/test_dualrun.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+echo "! setup !"
+
 module purge
-module load unstable python-dev py-neurodamus hpe-mpi
+module load unstable python-dev py-neurodamus py-mpi4py
+module load intel gcc hpe-mpi
 
 echo "*******************************************************************************"
 echo "STEPS_INSTALLED_HASH=${STEPS_INSTALLED_HASH}"
 echo "NEURODAMUS_NEOCORTEX_INSTALLED_HASH=${NEURODAMUS_NEOCORTEX_INSTALLED_HASH}"
 echo "*******************************************************************************"
+echo "Just loading already installed versions of steps & neurodamus."
 spack load /${STEPS_INSTALLED_HASH}
 spack load /${NEURODAMUS_NEOCORTEX_INSTALLED_HASH}
 
@@ -17,7 +21,35 @@ python -m venv ./python-venv
 source ./python-venv/bin/activate
 pip install psutil
 
+echo "Cloning Metabolism repo."
+rm -rf metabolismndam
+git clone --quiet -b main --single-branch https://bbpgitlab.epfl.ch/molsys/metabolismndam.git
+
+echo "[start] building custom special."
+rm -rf x86_64
+
+# legacy mod files for triplerun
+cp metabolismndam/custom_ndam_2021_02_22_archive202101/mod/* mod/
+
+# update the legacy ones with mod files from neurodamus-core
+rm -rf neurodamus-core/
+git clone --quiet -b main --single-branch https://bbpgitlab.epfl.ch/hpc/sim/neurodamus-core.git
+cp neurodamus-core/mod/* mod/
+rm -rf neurodamus-core/
+
+# additional mod files from common repo
+rm -rf common/
+git clone --quiet -b main --single-branch https://bbpgitlab.epfl.ch/hpc/sim/models/common.git
+cp -n common/mod/ngv/* mod/
+rm -rf common/
+
+build_neurodamus.sh mod
+echo "[finish] building custom special."
+
+# STEPS related
 export OMP_NUM_THREADS=1
+
+echo "! setup completed !"
 
 export which_STEPS=3
 PYDRIVER=multiscale_run_STEPS${which_STEPS}.py
