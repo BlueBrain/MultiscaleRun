07aad99402d6a2392bb6094ec82e0a6db053f5df
Christos Kotsalos <christos.kotsalos@epfl.ch>
2022-09-14 16:43:48 +0200

    TESTING CI

---
+++ .ci/test_triplerun.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+echo "! setup !"
+
+module purge
+module load unstable git python-dev py-neurodamus py-mpi4py
+module load intel gcc hpe-mpi
+
+echo "JULIA"
+ls ~/.julia
+
+rm -rf ~/.julia
+julia -e 'using Pkg; Pkg.add("IJulia")'
+julia -e 'using Pkg; Pkg.add("DifferentialEquations")'
+julia -e 'using Pkg; Pkg.add("DiffEqBase")'
+julia -e 'using Pkg; Pkg.add("ParameterizedFunctions")'
+julia -e 'using Pkg; Pkg.add("StaticArrays")'
+julia -e 'using Pkg; Pkg.add("RecursiveArrayTools")'
+julia -e 'using Pkg; Pkg.add("PyCall");Pkg.build("PyCall")'
+
+echo "JULIA : pushd/popd"
+pushd ~/.julia
+popd
+
+rm -rf ./python-venv
+python -m venv ./python-venv
+source ./python-venv/bin/activate
+
+pip install psutil
+pip install julia
+pip install diffeqpy
+pip install pympler
+pip install h5py
