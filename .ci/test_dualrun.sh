512aca47a7926a46c3453b894bec12912ac3673b
Christos Kotsalos <christos.kotsalos@epfl.ch>
2022-09-15 16:24:37 +0200

    CI : completed

--- .ci/test_dualrun.sh
+++ .ci/test_dualrun.sh
@@ -8,6 +8,17 @@ module purge
 module load unstable git python-dev py-neurodamus py-mpi4py
 module load intel gcc hpe-mpi
 
+echo "Cloning various repos."
+echo "metabolismndam"
+rm -rf metabolismndam/
+git clone --quiet -b main --single-branch https://gitlab-ci-token:${CI_JOB_TOKEN}@bbpgitlab.epfl.ch/molsys/metabolismndam.git
+echo "neurodamus-core"
+rm -rf neurodamus-core/
+git clone --quiet -b main --single-branch https://gitlab-ci-token:${CI_JOB_TOKEN}@bbpgitlab.epfl.ch/hpc/sim/neurodamus-core.git
+echo "common"
+rm -rf common/
+git clone --quiet -b main --single-branch https://gitlab-ci-token:${CI_JOB_TOKEN}@bbpgitlab.epfl.ch/hpc/sim/models/common.git
+
 echo "*******************************************************************************"
 echo "STEPS_INSTALLED_HASH=${STEPS_INSTALLED_HASH}"
 echo "NEURODAMUS_NEOCORTEX_INSTALLED_HASH=${NEURODAMUS_NEOCORTEX_INSTALLED_HASH}"
@@ -23,25 +34,41 @@ source ./python-venv/bin/activate
 
 pip install psutil
 
+echo "[...] building custom special."
+rm -rf x86_64
+
+# legacy mod files for triplerun
+cp metabolismndam/custom_ndam_2021_02_22_archive202101/mod/* mod/
+
+# update the legacy ones with mod files from neurodamus-core
+cp neurodamus-core/mod/* mod/
+rm -rf neurodamus-core/
+
+# additional mod files from common repo
+cp -n common/mod/ngv/* mod/
+rm -rf common/
+
+build_neurodamus.sh mod
+echo "[completed] building custom special."
+
 # STEPS related
 export OMP_NUM_THREADS=1
 
 echo "! setup completed !"
 
-# Remove Stimulus Inject from BC (else sum of currents != 0)
-sed -i 's/StimulusInject pInj/#StimulusInject pInj/g' BlueConfigCI
-
 export which_STEPS=3
 PYDRIVER=multiscale_run_STEPS${which_STEPS}.py
 export which_mesh=mc2c
 export which_BlueConfig=BlueConfigCI
+# Remove Stimulus Inject from BC (else sum of currents != 0)
+sed -i 's/StimulusInject pInj/#StimulusInject pInj/g' $which_BlueConfig
 # Like in C, the integers 0 (false) and 1 (true—in fact any nonzero integer) are used.
 export dualrun=1
 export triplerun=0
 echo "*******************************************************************************"
 echo " *** STEPS${which_STEPS} run *** "
 echo "*******************************************************************************"
-srun --overlap -n $bb5_ntasks dplace special -mpi -python ${PYDRIVER}
+srun --overlap -n $bb5_ntasks dplace x86_64/special -mpi -python ${PYDRIVER}
 
 echo "*******************************************************************************"
 echo " *** pytest *** "
--- .ci/test_triplerun_results.py
+++ .ci/test_triplerun_results.py
@@ -7,12 +7,15 @@ def test_ATPConc():
 
     ATPConc = None
     for line in notebook_html:
+        # Choose the line with the following format -> data_ATPConcAllCmps : mean=..., min=..., max=...
         if 'data_ATPConcAllCmps : ' in line:
             ATPConc = line
 
     assert ATPConc is not None
     
+    # Extract the numbers from the above-mentioned line
     ATPConc = np.array([float(x) for x in re.findall(r'[-+]?(?:\d*\.\d+|\d+)', ATPConc)])
 
+    # ATP should be between these values
     assert ATPConc[ATPConc < 1.0].size == 0
     assert ATPConc[ATPConc > 2.2].size == 0
