c3158947797f75aef09de84d4b44e2a920829a57
arnaudon <alexis.arnaudon@epfl.ch>
2023-11-17 15:56:00 +0100

    more

--- update_currents.sh
+++ update_currents.sh
@@ -9,13 +9,16 @@
 #SBATCH --job-name=current
 
 module load unstable py-mpi4py
-module load unstable neurodamus-neocortex
+module load unstable neurodamus-neocortex-multiscale
 module unload python
 
-srun emodel-generalisation -v compute_currents \
+srun \
+emodel-generalisation -v compute_currents \
     --input-path  /gpfs/bbp.cscs.ch/project/proj137/NGVCircuits/rat_sscx_S1HL/V10/build/sonata/networks/nodes/All/nodes.h5 \
     --output-path nodes.h5 \
     --morphology-path /gpfs/bbp.cscs.ch/project/proj83/entities/fixed-ais-L23PC-2020-12-10/ascii \
     --hoc-path  /gpfs/bbp.cscs.ch/project/proj137/NGVCircuits/rat_sscx_S1HL/V10/emodels/hoc \
     --protocol-config-path protocol_config.yaml \
+    --only-rin \
     --parallel-lib dask_dataframe
+#    --parallel-lib multiprocessing
