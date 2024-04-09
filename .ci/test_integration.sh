BlueBrain/MultiscaleRun

commit f683509b0a16e69a281e4be8b98da3e3c04bf4fe

rework metabolism [BBPP40-434, BBPP40-466, BBPP40-455, BBPP40-440, BBPP40-427, BBPP40-470]
This MR contains major reworks for:

* the config object
* the metabolism object
* the reporter object
* the connection object

Dan wanted to have a more scientific way of merging conflicting values from neurodamus and metabolism. This generated a cascade of events that made a lot of stuff obsolete or clunky to work with. For this reason I went forward and reworked a lot of stuff behind the scenes.

The initial aim was to get rid of all (-most?) the magic numbers springled here and there by Polina in the glue code. Now there are only 3 ways to sync data from a source simulation to a destination simulator:

* add: the values from the source are added to the destination
* set: the values from the source replace the values in destination
* merge: source and destination values are combined with this formula: V_source + V_destination - V_n-1 = V_n. Qualitatively here we sum the contributions from source and destination to get the final, merged value. This final value is then set for source and destination. Notice that swapping source and destination produces the same result. This is the only scheme that is flipping-invariant

Why did this generate so much change? Because a lot of stuff in the config file became unnecessary or complicated to maintain in the current form. Since we need also to merge msr_config inside simulation_config and make it sonata compliant I went forward and did both things.

Fix: BBPP40-434, BBPP40-466, BBPP40-455, BBPP40-440, BBPP40-427, BBPP40-470

Small changes:

* improved documentation
* updated postproc
* more pytest logging
* make simulations die correctly when an error is raised with mpi4py

TODO:

* [x] rat V10 move 400 -\> 4000 ndts for bf and metab
* [x] remove debug code: `# @utils.apply_abort_on_exception_decorator`
* [x] documentation for the new config file

Postponed to a future MR:

* BBPP40-471

.ci/test_integration.sh
@@ -7,8 +7,7 @@ set_test_environment
 
 pushd "${SCRIPT_DIR}/.." >/dev/null
 
-$MPIRUN -n 2 python tests/integration_tests/test_connect_ndam2steps.py
-$MPIRUN -n 2 python tests/integration_tests/test_ndam2steps_sync.py
+$MPIRUN -n 2 python tests/integration_tests/test_connect_neurodamus2steps.py
 $MPIRUN -n 2 python tests/integration_tests/test_autogen_mesh.py
 $MPIRUN -n 2 python tests/integration_tests/test_preprocessor.py
 

tests/integration_tests/test_connect_neurodamus2steps.py
@@ -60,7 +60,7 @@ def check_mats_shape(ndam_m, conn_m, steps_m, nshape=None, segshape=None):
     """
     d = {
         int(nc.CCell.gid): len(
-            [seg for sec in ndam_m._gen_secs(nc) for seg in ndam_m._gen_segs(sec)]
+            [seg for sec in ndam_m.gen_secs(nc) for seg in ndam_m.gen_segs(sec)]
         )
         for nc in ndam_m.ncs
     }
@@ -86,7 +86,9 @@ def check_mats_shape(ndam_m, conn_m, steps_m, nshape=None, segshape=None):
     np.testing.assert_equal(conn_m.nXnsegMatBool.shape, (nn, nseg))
 
 
-@utils.clear_and_replace_files_decorator([conf0.mesh_path.parent, conf0.cache_path])
+@utils.clear_and_replace_files_decorator(
+    [conf0.multiscale_run.mesh_path.parent, conf0.multiscale_run.cache_path]
+)
 @utils.logs_decorator
 def test_connection():
     """
@@ -99,17 +101,20 @@ def test_connection():
     conf = config.MsrConfig.rat_sscxS1HL_V6()
 
     prep = preprocessor.MsrPreprocessor(conf)
-
-    conn_m = connection_manager.MsrConnectionManager(conf)
+    managers = {}
+    conn_m = connection_manager.MsrConnectionManager(config=conf, managers=managers)
 
     prep.autogen_node_sets()
-    ndam_m = neurodamus_manager.MsrNeurodamusManager(conf)
-    conn_m.connect_ndam2ndam(ndam_m)
+    
+    managers["neurodamus"] = neurodamus_manager.MsrNeurodamusManager(conf)
+    conn_m.connect_neurodamus2neurodamus(managers["neurodamus"])
 
-    prep.autogen_mesh(ndam_m=ndam_m)
-    steps_m = steps_manager.MsrStepsManager(conf)
+    prep.autogen_mesh(ndam_m=managers["neurodamus"])
+    managers["steps"] = steps_manager.MsrStepsManager(conf)
 
-    conn_m.connect_ndam2steps(ndam_m=ndam_m, steps_m=steps_m)
+    conn_m.connect_neurodamus2steps(
+        ndam_m=managers["neurodamus"], steps_m=managers["steps"]
+    )
 
     check_ratio_mat(conn_m.nXtetMat)
     check_ratio_mat(conn_m.nsegXtetMat)
@@ -118,49 +123,75 @@ def test_connection():
     segshape = conn_m.nsegXtetMat.shape
     d = {
         int(nc.CCell.gid): len(
-            [seg for sec in ndam_m._gen_secs(nc) for seg in ndam_m._gen_segs(sec)]
+            [
+                seg
+                for sec in managers["neurodamus"].gen_secs(nc)
+                for seg in managers["neurodamus"].gen_segs(sec)
+            ]
         )
-        for nc in ndam_m.ncs
+        for nc in managers["neurodamus"].ncs
     }
 
-    utils.rank_print("gid: ", [int(nc.CCell.gid) for nc in ndam_m.ncs])
-    ndam_m.remove_gids(
-        failed_cells={933: "remove from rank 0", 1004: "remove from rank 1"},
-        conn_m=conn_m,
-    )
-    ndam_m.update(conn_m)
+    utils.rank_print("gid: ", [int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
+    failed_cells_dict = {933: "remove from rank 0", 1004: "remove from rank 1"}
+    failed_cells = [
+        failed_cells_dict.get(i, None) for i in managers["neurodamus"].gids
+    ]
+    conn_m.remove_gids(failed_cells=failed_cells, managers=managers)
 
-    utils.rank_print([int(nc.CCell.gid) for nc in ndam_m.ncs])
+    utils.rank_print([int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
 
     l = [v for k, v in d.items() if k in [933, 1004]]
     nshape = (nshape[0] - len(l), nshape[1])
     segshape = (segshape[0] - sum(l), segshape[1])
-    check_mats_shape(ndam_m, conn_m, steps_m, nshape=nshape, segshape=segshape)
+    check_mats_shape(
+        managers["neurodamus"],
+        conn_m,
+        managers["steps"],
+        nshape=nshape,
+        segshape=segshape,
+    )
     nshape = conn_m.nXtetMat.shape
     segshape = conn_m.nsegXtetMat.shape
 
-    utils.rank_print([int(nc.CCell.gid) for nc in ndam_m.ncs])
-
-    ndam_m.update(conn_m)
-    check_mats_shape(ndam_m, conn_m, steps_m, nshape=nshape, segshape=segshape)
-
-    utils.rank_print([int(nc.CCell.gid) for nc in ndam_m.ncs])
-
-    ndam_m.remove_gids(failed_cells={17500: "reason 0"}, conn_m=conn_m)
-    ndam_m.update(conn_m)
-    check_mats_shape(ndam_m, conn_m, steps_m, nshape=nshape, segshape=segshape)
-
-    utils.rank_print([int(nc.CCell.gid) for nc in ndam_m.ncs])
-
-    ndam_m.remove_gids(failed_cells={175: "reason 0"}, conn_m=conn_m)
-    ndam_m.update(conn_m)
+    utils.rank_print([int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
 
     check_mats_shape(
-        ndam_m,
+        managers["neurodamus"],
         conn_m,
-        steps_m,
+        managers["steps"],
+        nshape=nshape,
+        segshape=segshape,
+    )
+
+    utils.rank_print([int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
+
+    failed_cells_dict = {17500: "reason 0"}
+    failed_cells = [
+        failed_cells_dict.get(i, None) for i in managers["neurodamus"].gids
+    ]
+    conn_m.remove_gids(failed_cells=failed_cells, managers=managers)
+    check_mats_shape(
+        managers["neurodamus"],
+        conn_m,
+        managers["steps"],
+        nshape=nshape,
+        segshape=segshape,
+    )
+
+    utils.rank_print([int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
+
+    failed_cells_dict = {175: "reason 0"}
+    failed_cells = [
+        failed_cells_dict.get(i, None) for i in managers["neurodamus"].gids
+    ]
+
+    check_mats_shape(
+        managers["neurodamus"],
+        conn_m,
+        managers["steps"],
         nshape=nshape if not utils.rank0() else None,
-        segshape=segshape not utils.rank0() else None,
+        segshape=segshape if not utils.rank0() else None,
     )
 
 

.ci/test_pytest.sh
@@ -11,6 +11,9 @@ pushd "${SCRIPT_DIR}/.." >/dev/null
 num_errors=0
 count_errors() {
     ((num_errors++))
+    local command="$BASH_COMMAND"
+    echo "Error occurred while executing: $command (Trap: ERR)"
+    echo "num_errors: $num_errors"
 }
 trap count_errors ERR
 
@@ -18,11 +21,11 @@ trap count_errors ERR
 # related issue in: https://bbpteam.epfl.ch/project/issues/browse/BBPP40-412
 python -mpytest -v tests/pytests/test_bloodflow.py
 python -mpytest -v tests/pytests/test_cli.py
+python -mpytest -v tests/pytests/test_config.py
 python -mpytest -v tests/pytests/test_metabolism.py
 python -mpytest -v tests/pytests/test_neurodamus.py
 python -mpytest -v tests/pytests/test_steps.py
 python -mpytest -v tests/pytests/test_utils.py
-python -mpytest -v tests/pytests/test_config.py
 python -mpytest -v tests/pytests/test_preprocessor.py
 python -mpytest -v tests/pytests/test_reporter.py
 $MPIRUN -n 4 python -mpytest -v tests/pytests/test_reporter.py

.ci/test_simulation.sh
@@ -12,7 +12,7 @@ postproc=${postproc:true}
 steps=${steps:-false}
 metabolism=${metabolism:-false}
 bloodflow=${bloodflow:-false}
-sim_end=${sim_end:-1000}
+tstop=${tstop:-1000}
 
 if [ -z ${sim_name:x} ]; then
   fatal_error "expected environment variable 'SIM_NAME'."
@@ -22,11 +22,11 @@ rm -rf "$sim_name"
 multiscale-run init --no-check -f "$sim_name"
 
 pushd "$sim_name"
-/gpfs/bbp.cscs.ch/project/proj12/jenkins/subcellular/bin/jq ".with_steps = $steps | .with_bloodflow = $bloodflow | .with_metabolism = $metabolism | .msr_sim_end = $sim_end " msr_config.json > msr_config.json.bak
-mv msr_config.json.bak msr_config.json
-cat msr_config.json
+/gpfs/bbp.cscs.ch/project/proj12/jenkins/subcellular/bin/jq ".multiscale_run.with_steps = $steps | .multiscale_run.with_bloodflow = $bloodflow | .multiscale_run.with_metabolism = $metabolism | .run.tstop = $tstop " simulation_config.json > simulation_config.json.bak
+mv simulation_config.json.bak simulation_config.json
+cat simulation_config.json
 popd >/dev/null
-unset steps bloodflow metabolism sim_end
+unset steps bloodflow metabolism tstop
 
 module load unstable intel-oneapi-mkl gmsh
 srun --overlap -n $bb5_ntasks multiscale-run compute "$sim_name"

.gitignore
@@ -7,6 +7,7 @@ __pycache__/
 output/
 core.*
 *.log
+*.logs
 python-venv/
 *.dat
 x86_64/

.gitlab-ci.yml
@@ -81,21 +81,24 @@ dualrun_test 1/16:
   variables:
     bb5_ntasks: 1
     bb5_duration: "02:00:00"
-    sim_end: 105
+    tstop: 105
 
 dualrun_test 2/16:
   extends: .dualrun_test
   variables:
     bb5_ntasks: 2
-    sim_end: 105
+    tstop: 105
 
 dualrun_test 16/16:
   extends: .dualrun_test
   variables:
     bb5_ntasks: 16
+    tstop: 1000
 
 nrun_[steps, metab, bf]:
   extends: .nrun_test
+  variables:
+    tstop: 500
   parallel:
     matrix:
     - steps: ["false", "true"]

multiscale_run/bloodflow_manager.py
@@ -24,7 +24,7 @@ class MsrBloodflowManager:
         graph (PointVasculature): The vasculature graph.
     """
 
-    def __init__(self, vasculature_path, params):
+    def __init__(self, vasculature_path, parameters):
         """
         Initialize the MsrBloodflowManager.
 
@@ -34,12 +34,12 @@ class MsrBloodflowManager:
 
         Args:
             vasculature_path (str): The path to the vasculature data file.
-            params (dict): A dictionary containing parameters for bloodflow calculations.
+            parameters (dict): A dictionary containing parameters for bloodflow calculations.
         """
 
         logging.info("init MsrBloodflowManager")
 
-        self.params = params
+        self.parameters = parameters
 
         self.graph = None
         if utils.rank0():
@@ -104,14 +104,14 @@ class MsrBloodflowManager:
         Get bloodflow input nodes (entry nodes).
 
         This method determines the bloodflow input nodes in the vasculature graph. It identifies these
-        nodes based on criteria defined by the 'params' provided when initializing the MsrBloodflowManager.
+        nodes based on criteria defined by the 'parameters' provided when initializing the MsrBloodflowManager.
 
         Returns:
             list: A list of bloodflow input nodes as node IDs.
         """
 
         self.entry_nodes = create_entry_largest_nodes(
-            graph=self.graph, params=self.params
+            graph=self.graph, params=self.parameters
         )
         logging.info(f"entry nodes: {self.entry_nodes}")
 
@@ -156,7 +156,7 @@ class MsrBloodflowManager:
 
         input_flows = None
         if self.entry_nodes is not None:
-            input_flows = [self.params["input_v"]] * len(self.entry_nodes)
+            input_flows = [self.parameters["input_v"]] * len(self.entry_nodes)
         self.boundary_flows = bloodflow.boundary_flows_A_based(
             graph=self.graph, entry_nodes=self.entry_nodes, input_flows=input_flows
         )
@@ -178,42 +178,37 @@ class MsrBloodflowManager:
         bloodflow.update_static_flow_pressure(
             graph=self.graph,
             input_flow=self.boundary_flows,
-            blood_viscosity=self.params["blood_viscosity"],
-            base_pressure=self.params["base_pressure"],
+            params=self.parameters,
         )
 
     @utils.logs_decorator
-    def set_radii(self, vasc_ids, radii):
+    def set_radii(self, idxs: list[int], vals: list[float]) -> None:
         """
         Set radii for vasculature sections.
 
         This method allows you to set radii for specific vasculature sections identified by their indices.
-        It takes care of distributing the radii evenly across the sections to ensure that the total volume is maintained.
 
         Args:
-            vasc_ids (List[int]): A list of vasculature section indices to set the radii for.
-            radii (List[float]): A list of radii corresponding to the specified vasculature sections.
+            idxs: A list of vasculature section indices to set the radii for.
+            vals: A list of radii corresponding to the specified vasculature sections.
 
         Returns:
             None
 
         Note:
             - The method calculates equivalent radii to distribute across the specified sections while maintaining volume.
-            - It ensures that radii are evenly distributed among sections with the same index.
             - The updated radii are stored in the vasculature graph's edge properties.
         """
 
         def eq_radii(v):
-            v = np.array(v)
             r"""compute x from: 1/x^2 = \sum_n 1/(n*r_i^2)"""
+            v = np.array(v)
             return np.sqrt(v.dot(v) / len(v))
-
-        vasc_ids = [self.graph.edge_properties.index[i] for i in vasc_ids]
 
         # I did not find a more pythonic way of doing this. I am open to suggestions
         d = defaultdict(list)
-        for k, v in zip(vasc_ids, radii):
-            d[k].append(v)
+        for idx, v in zip(idxs, vals):
+            d[self.graph.edge_properties.index[idx]].append(v)
 
         d = {k: eq_radii(v) for k, v in d.items()}
         # without interventions, d.keys() and d.values() are ordered in the samw way

multiscale_run/cli.py
@@ -18,6 +18,7 @@ import subprocess
 import sys
 import tempfile
 import textwrap
+import traceback
 
 from nbconvert.nbconvertapp import main as NbConvertApp
 import diffeqpy
@@ -142,7 +143,7 @@ def init(directory, circuit, julia="shared", check=True, force=False):
 
     circuit = circuit_path(circuit)
 
-    SBATCH_CIRCUITS_PARAMS = dict(
+    SBATCH_CIRCUITS_parameters = dict(
         rat_sscxS1HL_V6=dict(
             job_name="msr_ratV6",
             nodes=1,
@@ -154,12 +155,12 @@ def init(directory, circuit, julia="shared", check=True, force=False):
             time="10:00:00",
         ),
     )
-    sbatch_params = copy.copy(SBATCH_CIRCUITS_PARAMS[circuit.name])
+    sbatch_parameters = copy.copy(SBATCH_CIRCUITS_parameters[circuit.name])
     loaded_modules = filter(
         lambda s: len(s) > 0, os.environ.get("LOADEDMODULES", "").split(":")
     )
-    sbatch_params["loaded_modules"] = loaded_modules
-    SBATCH_TEMPLATE.stream(sbatch_params).dump("simulation.sbatch")
+    sbatch_parameters["loaded_modules"] = loaded_modules
+    SBATCH_TEMPLATE.stream(sbatch_parameters).dump("simulation.sbatch")
     shutil.copy(MSR_POSTPROC, MSR_POSTPROC.name)
     shutil.copytree(
         str(circuit),
@@ -167,6 +168,7 @@ def init(directory, circuit, julia="shared", check=True, force=False):
         ignore=shutil.ignore_patterns("cache", "msr*"),
         dirs_exist_ok=True,
     )
+
     rd = {"msr_version": str(__version__)}
     if julia == "no":
         rd["with_metabolism"] = False
@@ -213,7 +215,9 @@ def init(directory, circuit, julia="shared", check=True, force=False):
             check_julia_env()
 
     MsrConfig.dump(
-        config_path=MSR_CONFIG_JSON, to_path=MSR_CONFIG_JSON.name, replace_dict=rd
+        config_path=circuit / MSR_CONFIG_JSON,
+        to_path=MSR_CONFIG_JSON,
+        replace_dict={"multiscale_run": rd},
     )
 
     LOGGER.warning(
@@ -247,7 +251,7 @@ def check(**kwargs):
 
     sane &= _check_local_neuron_mechanisms()
 
-    if sim.conf.with_metabolism:
+    if sim.conf.multiscale_run.with_metabolism:
         LOGGER.warning("Checking installation of differential equations solver...")
         # noinspection PyUnresolvedReferences
         from diffeqpy import de  # noqa : F401
@@ -268,7 +272,8 @@ def post_processing(notebook: str, **kwargs) -> Path:
     """
     from .config import MsrConfig
 
-    results = MsrConfig().results_path
+    conf = MsrConfig()
+    results = str(conf.output.output_dir)
     NbConvertApp(
         ["--execute", "--to", "html", "--no-input", f"--output-dir={results}", notebook]
     )
@@ -660,6 +665,9 @@ def main(**kwargs):
     Args:
         kwargs: optional arguments passed to the argument parser.
     """
+
+    from .utils import comm, size
+
     ap = argument_parser()
     args = ap.parse_args(**kwargs)
     args = vars(args)
@@ -675,8 +683,11 @@ def main(**kwargs):
     if callback := args.pop("func", None):
         try:
             callback(**args)
-        except MsrException as e:
+        except Exception as e:
             LOGGER.error(str(e))
-            sys.exit(1)
+            if size() > 1:
+                comm().Abort(errorcode=1)
+            else:
+                sys.exit(1)
     else:
         ap.error("a subcommand is required.")

commit 41c5120a38bf1f256f4fad39fe675ea32262bd1f

Fix regression recently introduced in f683509b0a16e69a281e4be8b98da3e3c04bf4fe
Also ensure silent errors won't happen again in integration tests

.ci/test_integration.sh
@@ -7,8 +7,18 @@ set_test_environment
 
 pushd "${SCRIPT_DIR}/.." >/dev/null
 
+num_errors=0
+count_errors() {
+    ((num_errors++))
+    local command="$BASH_COMMAND"
+    echo "Error occurred while executing: $command (Trap: ERR)"
+    echo "num_errors: $num_errors"
+}
+trap count_errors ERR
+
 time $MPIRUN -n 2 python tests/integration_tests/test_connect_neurodamus2steps.py
 time $MPIRUN -n 2 python tests/integration_tests/test_autogen_mesh.py
 time $MPIRUN -n 2 python tests/integration_tests/test_preprocessor.py
 
 popd > /dev/null
+exit $num_errors

tests/integration_tests/test_connect_neurodamus2steps.py
@@ -101,14 +101,12 @@ def test_connection():
     prep.autogen_node_sets()
 
     managers["neurodamus"] = MsrNeurodamusManager(conf)
-    conn_m.connect_neurodamus2neurodamus(managers["neurodamus"])
+    conn_m.connect_neurodamus2neurodamus()
 
     prep.autogen_mesh(ndam_m=managers["neurodamus"])
     managers["steps"] = MsrStepsManager(conf)
 
-    conn_m.connect_neurodamus2steps(
-        ndam_m=managers["neurodamus"], steps_m=managers["steps"]
-    )
+    conn_m.connect_neurodamus2steps()
 
     check_ratio_mat(conn_m.nXtetMat)
     check_ratio_mat(conn_m.nsegXtetMat)
@@ -129,7 +127,7 @@ def test_connection():
     utils.rank_print("gid: ", [int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
     failed_cells_dict = {933: "remove from rank 0", 1004: "remove from rank 1"}
     failed_cells = [failed_cells_dict.get(i, None) for i in managers["neurodamus"].gids]
-    conn_m.remove_gids(failed_cells=failed_cells, managers=managers)
+    conn_m.remove_gids(failed_cells=failed_cells)
 
     utils.rank_print([int(nc.CCell.gid) for nc in managers["neurodamus"].ncs])
 
@@ -160,7 +158,7 @@ def test_connection():
 
     failed_cells_dict = {17500: "reason 0"}
     failed_cells = [failed_cells_dict.get(i, None) for i in managers["neurodamus"].gids]
-    conn_m.remove_gids(failed_cells=failed_cells, managers=managers)
+    conn_m.remove_gids(failed_cells=failed_cells)
     check_mats_shape(
         managers["neurodamus"],
         conn_m,