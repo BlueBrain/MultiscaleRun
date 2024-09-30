e074bf6db626f7859a19d99d3e7a24e35da8d365
katta <alessandro.cattabiani@epfl.ch>
2024-09-30 11:22:44 +0200

    remove debug code

--- tests/pytests/test_steps.py
+++ tests/pytests/test_steps.py
@@ -76,7 +76,8 @@ def test_steps_with_minimesh(tmp_path):
 
 if __name__ == "__main__":
     tmp_path = "tmp"
-    # test_steps_with_minimesh()
+    utils.remove_path(tmp_path)
+    test_steps_with_minimesh()
     utils.remove_path(tmp_path)
     test_steps_connections_mats(tmp_path)
-    # utils.remove_path(tmp_path)
+    utils.remove_path(tmp_path)
