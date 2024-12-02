496e2230f217676d2a9d3f012437de03c56bd0c5
katta <alessandro.cattabiani@epfl.ch>
2024-12-02 17:00:17 +0100

    WIP2

--- .ci/test_integration.sh
+++ .ci/test_integration.sh
@@ -9,24 +9,18 @@ pushd "${SCRIPT_DIR}/.." >/dev/null
 
 download_tiny_CI_neurodamus_data
 
-echo "here I am $(pwd)"
-echo "what is here?"
-ls
-echo "what is in multiscale_run/templates/tiny_CI/"
-ls multiscale_run/templates/tiny_CI/
+num_errors=0
+count_errors() {
+    ((num_errors++))
+    local command="$BASH_COMMAND"
+    echo "Error occurred while executing: $command (Trap: ERR)"
+    echo "num_errors: $num_errors"
+}
+trap count_errors ERR
 
-# num_errors=0
-# count_errors() {
-#     ((num_errors++))
-#     local command="$BASH_COMMAND"
-#     echo "Error occurred while executing: $command (Trap: ERR)"
-#     echo "num_errors: $num_errors"
-# }
-# trap count_errors ERR
+time $MPIRUN -n 2 python tests/integration/connect_neurodamus2steps.py
+time $MPIRUN -n 2 python tests/integration/autogen_mesh.py
+time $MPIRUN -n 2 python tests/integration/preprocessor.py
 
-# time $MPIRUN -n 2 python tests/integration/connect_neurodamus2steps.py
-# time $MPIRUN -n 2 python tests/integration/autogen_mesh.py
-# time $MPIRUN -n 2 python tests/integration/preprocessor.py
-
-# popd > /dev/null
-# exit $num_errors
+popd > /dev/null
+exit $num_errors
